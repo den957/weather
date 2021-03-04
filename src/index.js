c422d1e3886d68e6b0d1622c3976c0a627fa83cb
Dan 957 <denyshorban999@gamil.com>
2021-03-04 14:01:38 +0100

    Optimize reload app on GitHub page

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import store from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter >
+  <HashRouter >
     <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
     </React.StrictMode>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 )
 reportWebVitals();
