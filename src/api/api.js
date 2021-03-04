8f85c531a4dc3d1a0cf8242d3a4d0822daf87c01
Dan 957 <denyshorban999@gamil.com>
2021-03-04 13:52:23 +0100

    Remove issue "Mixed Content..."

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
-
 export const weatherApi = {
    weatherInfo(dataCity) {
-      return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${dataCity}&appid=9f2486a52618766bf36ec0fa97d8385d`)
+      if (window.location.protocol === 'http:') {
+         let url = 'http://api.openweathermap.org/data/2.5/'
+         return axios.get(`${url}weather?q=${dataCity}&appid=9f2486a52618766bf36ec0fa97d8385d`)
+      } else {
+         let url = 'https://api.openweathermap.org/data/2.5/'
+         return axios.get(`${url}weather?q=${dataCity}&appid=9f2486a52618766bf36ec0fa97d8385d`)
+      }
    }
 }
